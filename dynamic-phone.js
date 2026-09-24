fe01a66f11aeca9e277cbc83c487459a672607fb
jasondanielhobbs-product <jasondanielhobbs@gmail.com>
2026-09-23 17:05:21 -0700

    Create dynamic-phone.js

---
+++ dynamic-phone.js
@@ -0,0 +1,8 @@
+/**
+ * Dynamic Phone Number Insertion
+ *
+ * Maps traffic sources to phone numbers.
+ * The Wix page itself should always contain the default phone number
+ * so a working number remains visible even if JavaScript fails.
+ */
+
